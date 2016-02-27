0dcd0897c68a6c40a64c9ac76a8a192be43711a6
Cole R Lawrence <cole@zombiehippie.com>
2016-02-27 16:04:28 -0600

    Add some comments on the room.js for what we need to do

--- routes/room.js
+++ routes/room.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
   var rid = req.params.id
+  // TODO use rid to lookup connection information of peers
+  // if rid does not match an available connection, send to lobby with message
+  // if rid does match send proper information
   res.render('room', {
     title: rid + ' - Cream Room',
     roomId: rid,
