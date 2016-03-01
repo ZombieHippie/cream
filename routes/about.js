f8095de1d9620de521edc1480766373b03991a6d
Kory Rekowski <krekowski12@gmail.com>
2016-02-29 18:09:55 -0600

    You spelled my name wrong.

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Team members: David, Cole, Matt, Cory, Cameron');
+  res.send('Team members: David, Cole, Matt, Kory, Cameron');
 });
 
 module.exports = router;
