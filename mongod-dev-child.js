d5f0ec68fd73cfa2e0f951cb63b39aa067d24fac
Cole R Lawrence <Colelawr@gmail.com>
2016-04-13 17:00:29 -0500

    Troubleshoot Travis-CI 3 - Mkdir db-log

--- mongod-dev-child.js
+++ mongod-dev-child.js
@@ -6,7 +6,7 @@ var node_cmd = process.env.NODE_EXEC || 'node'
 exports.start = function StartMongod(mongodb_port, db_dir) {
   var mongod_cmd = node_cmd
   var mongod_js = path.join(__dirname, 'node_modules/mongodb-prebuilt/binjs', 'mongod.js')
-  var mongod_args = [ mongod_js, '--dbpath=' + db_dir, '--logpath=test-db-log/log', '--port=' + mongodb_port]
+  var mongod_args = [ mongod_js, '--dbpath=' + db_dir, '--logpath=' + db_dir + '-log/log', '--port=' + mongodb_port]
 
   return proc.spawn(mongod_cmd, mongod_args, {
     stdio: 'inherit',
