93e5efdd2bf566cfdcc7742f2161d0db6e401590
Cole R Lawrence <Colelawr@gmail.com>
2016-04-14 16:16:55 -0500

    Travis-CI Debugging 4 silent false

--- nightwatch.js
+++ nightwatch.js
@@ -34,7 +34,7 @@ module.exports = {
       "launch_url" : "http://localhost",
       "selenium_port"  : 4444,
       "selenium_host"  : "localhost",
-      "silent": true,
+      "silent": false,
       "screenshots" : {
         "enabled" : !travis_ci,
         "path" : "./reports"
