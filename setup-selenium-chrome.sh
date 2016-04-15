5310bcaefc7c7bc4850acdffc1d255b1e87a1992
Cole R Lawrence <Colelawr@gmail.com>
2016-04-14 19:19:31 -0500

    3 rm sudo

--- setup-selenium-chrome.sh
+++ setup-selenium-chrome.sh
@@ -4,20 +4,20 @@
 # (Tested on Ubuntu 12.04)
 
 # Add Google Chrome's repo to sources.list
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | tee -a /etc/apt/sources.list
 
 # Install Google's public key used for signing packages (e.g. Chrome)
 # (Source: http://www.google.com/linuxrepositories/)
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
 # Update apt sources:
-sudo apt-get update
+apt-get update
 
 # Install Python, pip, Selenium:
-sudo apt-get -y install python python-pip
-sudo pip install selenium
+apt-get -y install python python-pip
+pip install selenium
 
-sudo apt-get -y install unzip
+apt-get -y install unzip
 # Download/Install chromedriver
 # (http://code.google.com/p/chromedriver/):
 #
@@ -30,19 +30,19 @@ wget -c http://chromedriver.googlecode.com/files/chromedriver_linux64_2.1.zip
 unzip chromedriver_linux64_2.1.zip
 # Neither creating symbolic link nor adding dir to $PATH worked for me
 # in a Vagrant VM. So, let's blatantly copy the binary:
-sudo cp ./chromedriver /usr/bin/
-sudo chmod ugo+rx /usr/bin/chromedriver
+cp ./chromedriver /usr/bin/
+chmod ugo+rx /usr/bin/chromedriver
 
 # Install Google Chrome:
-sudo apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4
-sudo apt-get -y install google-chrome-stable
+apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4
+apt-get -y install google-chrome-stable
 
 # Dependencies to make "headless" chrome/selenium work:
-sudo apt-get -y install xvfb gtk2-engines-pixbuf
-sudo apt-get -y install xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
+apt-get -y install xvfb gtk2-engines-pixbuf
+apt-get -y install xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
 
 # Optional but nifty: For capturing screenshots of Xvfb display:
-sudo apt-get -y install imagemagick x11-apps
+apt-get -y install imagemagick x11-apps
 
 # Make sure that Xvfb starts everytime the box/vm is booted:
 echo "Starting X virtual framebuffer (Xvfb) in background..."
