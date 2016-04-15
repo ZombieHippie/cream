44cdd9d5e98fb73d2f63b0a3af2c0225447501ad
Cole R Lawrence <Colelawr@gmail.com>
2016-04-14 22:23:30 -0500

    :arrow_up: Attempt to update selenium server to 2.53.0

--- nightwatch.js
+++ nightwatch.js
@@ -21,7 +21,7 @@ module.exports = {
 
   "selenium" : {
     "start_process" : true,
-    "server_path" : "bin/selenium-server-standalone-2.44.0.jar",
+    "server_path" : "bin/selenium-server-standalone-2.53.0.jar",
     "log_path" : "",
     "host" : "127.0.0.1",
     "port" : "4444",
